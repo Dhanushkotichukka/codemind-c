109a4fc5bf238f6e72ca9e1b85aac19d1e55719a
Dhanushkoti Chukka <147791860+Dhanushkotichukka@users.noreply.github.com>
2023-11-11 10:45:38 +0530

    #include<stdio.h> int main() {     int E,M,P,Ch,C;     scanf("%d%d%d%d%d",&E,&M,&P,&Ch,&C);     if(E>34 && M>34 && P>34 && Ch>34 && C>34)     printf("PASSED");     else     printf("FAILED");
    }

---
+++ Passed_or_Failed.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int E,M,P,Ch,C;
+    scanf("%d%d%d%d%d",&E,&M,&P,&Ch,&C);
+    if(E>34 && M>34 && P>34 && Ch>34 && C>34)
+    printf("PASSED");
+    else
+    printf("FAILED");
+    
+}
