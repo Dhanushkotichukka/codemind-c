28291fa3855d845e51d3317a08cbf2476494cc58
Dhanushkoti Chukka <147791860+Dhanushkotichukka@users.noreply.github.com>
2023-12-12 22:45:22 +0530

    #include<stdio.h> int main() {     int a,i,j;     scanf("%d",&a);     for(i=a;i>=1;i--)     {         for(j=1;j<=a;j++)         {             if(j<=i)             {                 printf("%d",i);             }             else             {                 printf("*");             }         }         printf(" ");     } }

---
+++ Basic_Pattern_5.3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j;
+    scanf("%d",&a);
+    for(i=a;i>=1;i--)
+    {
+        for(j=1;j<=a;j++)
+        {
+            if(j<=i)
+            {
+                printf("%d",i);
+            }
+            else
+            {
+                printf("*");
+            }
+        }
+        printf("
+");
+    }
+}
